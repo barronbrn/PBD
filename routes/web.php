c8435b71ccf7f2e6c4fe472c0b4d03fca12b6832
barronbrn <qwebaron7@gmail.com>
2025-02-09 00:39:11 +0700

    modelcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ObatController;
+use App\Http\Controllers\FakturController;
+use App\Http\Controllers\ApotekerController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\FakturObatController;
+use App\Http\Controllers\PembayaranController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// CRUD Routes
+Route::resource('obat', ObatController::class);
+Route::resource('pelanggan', PelangganController::class);
+Route::resource('faktur', FakturController::class);
+Route::resource('apoteker', ApotekerController::class);
+Route::resource('pembayaran', PembayaranController::class);
+Route::resource('faktur_obat', FakturObatController::class);
